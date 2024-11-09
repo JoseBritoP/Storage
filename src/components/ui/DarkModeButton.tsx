8c3db16a99bd722a36aa53a1e383c62901ccfe49
José Brito <josealbritose@gmail.com>
2024-11-09 19:51:58 -0300

    Style Butotn

--- src/components/ui/DarkModeButton.tsx
+++ src/components/ui/DarkModeButton.tsx
@@ -9,11 +9,11 @@ export default function DarkModeButton() {
   const { theme, setTheme } = useTheme()
   return (
     <div className='flex  justify-center items-center gap-x-4 w-full px-3'>
-      <Button onClick={()=>setTheme('light')} className={`${theme === 'light' ? 'border bg-brand-100 hover:bg-rose-500 ' : 'bg-gray-100 text-light-100 dark:bg-dark-200 dark:text-light-300 rounded-lg hover:bg-gray-300/50 dark:hover:bg-gray-800'} transition-colors duration-150 ease-in-out `}>
+      <Button onClick={()=>setTheme('light')} className={`${theme === 'light' ? 'border bg-brand-100 hover:bg-rose-500 ' : 'bg-gray-100 text-light-100 dark:bg-dark-200 dark:text-light-300 rounded-lg hover:bg-gray-300/50 dark:hover:bg-gray-800'} transition-colors duration-150 ease-in-out rounded-full shadow-md dark:shadow-gray-800 px-10 py-6 `}>
         <SunIcon/>
         <p>Light Mode</p>
       </Button>
-      <Button onClick={()=>setTheme('dark')} className={`${theme === 'dark' ? 'border bg-brand/80 hover:bg-rose-500 text-light-300 ' : 'bg-gray-100 text-light-100 dark:bg-dark-200 dark:text-light-300 rounded-lg hover:bg-gray-300/50 dark:hover:bg-gray-800'} transition-colors duration-150 ease-in-out `}>
+      <Button onClick={()=>setTheme('dark')} className={`${theme === 'dark' ? 'border bg-brand/80 hover:bg-rose-500/75 text-light-300 ' : 'bg-gray-100 text-light-100 dark:bg-dark-200 dark:text-light-300 rounded-lg hover:bg-gray-300/50 dark:hover:bg-gray-800'} transition-colors duration-150 ease-in-out rounded-full shadow-md dark:shadow-gray-800 px-10 py-6 `}>
         <p>Dark Mode</p>
         <MoonIcon/>
       </Button>
