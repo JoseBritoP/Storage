5f316d81660226b10c33eb684d85f384a8158906
José Brito <josealbritose@gmail.com>
2024-11-09 03:45:55 -0300

    config appwrite

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,3 +1,19 @@
+const checkAppwrite = {
+  endpoint:process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
+  project:process.env.NEXT_PUBLIC_APPWRITE_PROJECT,
+  database:process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
+  usersCollection:process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION,
+  filesCollection:process.env.NEXT_PUBLIC_APPWRITE_FILES_COLLECTION,
+  bucket:process.env.NEXT_PUBLIC_APPWRITE_BUCKET,
+  secret:process.env.NEXT_APPWRITE_KEY
+}
+
+Object.entries(checkAppwrite).forEach(([key, value]) => {
+  if (!value) {
+    throw new Error(`The value of ${key} is empty or undefined.`);
+  }
+});
+
 export const appwriteConfig = {
   endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!, 
   projectId:process.env.NEXT_PUBLIC_APPWRITE_PROJECT!,
