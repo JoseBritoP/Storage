36d11e8a95414d16489b3f2bdd0540157c78ce31
José Brito <josealbritose@gmail.com>
2024-11-09 04:12:54 -0300

    Root layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,3 +1,6 @@
+import Header from "@/components/shared/Header";
+import MobileNav from "@/components/shared/MobileNav";
+import Sidebar from "@/components/shared/Sidebar";
 import React from "react";
 
 export default function RootLayout({
@@ -7,10 +10,11 @@ export default function RootLayout({
 }) {
   return (
     <main className="flex h-screen">
-      Sidebar
+      <Sidebar />
       <section className="flex h-full flex-1 flex-col">
-        MobileNav Header
-        <div className="main-content">{children }</div>
+        <MobileNav />
+        <Header />
+        <div className="main-content">{children}</div>
       </section>
     </main>
   );
