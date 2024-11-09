63b05a51c24d283e7a6b3abe7c1fc98c217ae780
José Brito <josealbritose@gmail.com>
2024-11-09 14:25:23 -0300

    header

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,20 +1,41 @@
-import Image from 'next/image'
-import React from 'react'
-import Search from './Header/Search'
-import FileUplodeader from './Header/FileUplodeader'
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { signOutUser } from "@/lib/actions/user.actions";
+import Search from "./Header/Search";
+import FileUplodeader from "./Header/FileUplodeader";
 
-export default function Header() {
+const Header = ({
+  userId,
+  accountId,
+}: {
+  userId: string;
+  accountId: string;
+}) => {
   return (
-    <header className='header'>
-      <Search/>
-      <div className='header-wrapper'>
-        <FileUplodeader/>
-        <form action="">
-          <button type='submit' className='sign-out-button'>
-            <Image alt='logout' src='/assets/icons/logout.svg' height={24} width={24} className='w-6'/>
-          </button>
+    <header className="header">
+      <Search />
+      <div className="header-wrapper">
+        <FileUplodeader ownerId={userId} accountId={accountId} />
+        <form
+          action={async () => {
+            "use server";
+
+            await signOutUser();
+          }}
+        >
+          <Button type="submit" className="sign-out-button">
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="logo"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
         </form>
       </div>
     </header>
-  )
-}
+  );
+};
+export default Header;
