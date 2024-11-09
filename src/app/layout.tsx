48f67c99903c385220ade363d536174f99578ac1
José Brito <josealbritose@gmail.com>
2024-11-09 03:10:16 -0300

    ToastifyNotification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import ToastNotification from "@/components/ui/ToastifyNotification";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -29,6 +30,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <ToastNotification/>
       </body>
     </html>
   );
