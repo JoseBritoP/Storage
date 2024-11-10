92009c1c6fdced74cdc9458f78b832bf922bbb54
José Brito <josealbritose@gmail.com>
2024-11-09 21:22:07 -0300

    FileUploader

--- src/hooks/useFileUploader.tsx
+++ src/hooks/useFileUploader.tsx
@@ -1,54 +1,66 @@
-"use client"
-import { MAX_FILE_SIZE } from '@/constants';
-import { uploadFile } from '@/lib/actions/file.actions';
-import { usePathname } from 'next/navigation';
-import React, { useCallback, useState } from 'react'
-import { toast } from 'react-toastify';
-import { FileUploaderProps } from '@/components/shared/Header/FileUplodeader';
-import { useDropzone } from 'react-dropzone';
+"use client";
+import { MAX_FILE_SIZE } from "@/constants";
+import { uploadFile } from "@/lib/actions/file.actions";
+import { usePathname } from "next/navigation";
+import React, { useCallback, useState } from "react";
+import { toast } from "react-toastify";
+import { FileUploaderProps } from "@/components/shared/Header/FileUploder";
+import { useDropzone } from "react-dropzone";
 
-export default function useFileUploader({accountId,ownerId}:FileUploaderProps) {
+export default function useFileUploader({
+  accountId,
+  ownerId,
+}: FileUploaderProps) {
   const path = usePathname();
   const [files, setFiles] = useState<File[]>([]);
 
   const onDrop = useCallback(
     async (acceptedFiles: File[]) => {
       setFiles(acceptedFiles);
-
       const uploadPromises = acceptedFiles.map(async (file) => {
         if (file.size > MAX_FILE_SIZE) {
           setFiles((prevFiles) =>
-            prevFiles.filter((f) => f.name !== file.name),
+            prevFiles.filter((f) => f.name !== file.name)
           );
-        
-          toast.error(`${file.name} is too large. Max file size is 50MB`)
+          toast.error(`${file.name} is too large. Max file size is 50MB`);
+          return;
         }
 
-        return uploadFile({ file, ownerId, accountId, path }).then(
-          (uploadedFile) => {
-            if (uploadedFile) {
-              setFiles((prevFiles) =>
-                prevFiles.filter((f) => f.name !== file.name),
-              );
-            }
-          },
-        );
+        try {
+          // Intentar subir el archivo
+          const uploadedFile = await uploadFile({
+            file,
+            ownerId,
+            accountId,
+            path,
+          });
+
+          if (uploadedFile) {
+            setFiles((prevFiles) =>
+              prevFiles.filter((f) => f.name !== file.name)
+            );
+            toast.success(`${file.name} was successfully uploaded!`); // Éxito
+          }
+        } catch (error) {
+          console.log("Failed to upload", error);
+          toast.error(`Failed to upload ${file.name}. Please try again.`);
+        }
       });
 
       await Promise.all(uploadPromises);
     },
-    [ownerId, accountId, path],
+    [ownerId, accountId, path]
   );
 
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   const handleRemoveFile = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-    fileName: string,
+    fileName: string
   ) => {
     e.stopPropagation();
     setFiles((prevFiles) => prevFiles.filter((file) => file.name !== fileName));
   };
 
-  return { files, getRootProps, getInputProps, handleRemoveFile }
+  return { files, getRootProps, getInputProps, handleRemoveFile };
 }
